f6360b76c277e62c3f6923001b2cdcf96e03c64b
jcy0417 <jcy0417@shinsung.co.kr>
2019-07-10 08:41:43 +0900

    create

---
+++ WCF_Tcp_Client/WCF_Tcp_Client/WCFTcpClientForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WCF_Tcp_Client
+{
+    partial class WCFTcpClientForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.listBoxUsers = new System.Windows.Forms.ListBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(713, 10);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "전송";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(201, 12);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(506, 21);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // listBoxUsers
+            // 
+            this.listBoxUsers.FormattingEnabled = true;
+            this.listBoxUsers.ItemHeight = 12;
+            this.listBoxUsers.Location = new System.Drawing.Point(12, 12);
+            this.listBoxUsers.Name = "listBoxUsers";
+            this.listBoxUsers.Size = new System.Drawing.Size(183, 424);
+            this.listBoxUsers.TabIndex = 2;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonConnect.Location = new System.Drawing.Point(594, 384);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(94, 52);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.buttonDisconnect.Location = new System.Drawing.Point(694, 384);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(94, 52);
+            this.buttonDisconnect.TabIndex = 0;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // WCFTcpClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxUsers);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "WCFTcpClientForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.ListBox listBoxUsers;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
+
---
+++ WCF_Tcp_Client/WCF_Tcp_Client/WCFTcpClientForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using WCF_Tcp_Client.WCFTcpServer;
+
+namespace WCF_Tcp_Client
+{
+    public partial class WCFTcpClientForm : Form
+    {
+        WCFTcpServerClient serverHost = null;
+        static WCFTcpClientForm MainForm = null;
+
+        public WCFTcpClientForm()
+        {
+            InitializeComponent();
+
+            MainForm = this;
+        }
+
+        void ConnectWCFService()
+        {
+            NetTcpBinding bindig = new NetTcpBinding();
+            bindig.Security.Mode = SecurityMode.None;
+            bindig.ReliableSession.Enabled = false;
+
+            //콜백 인스턴스를 구현한 클래스를 아래와 같이 적용한다;
+            InstanceContext ict = new InstanceContext(new ServerCallbackHandler());
+
+            /*SomeClient 는 서비스참조에 의해 자동으로 생성된 클래스로서
+                DuplexClientBase<ISome> 을 상속하고
+                ISome 을 구현 한다
+                (정의로 이동) 하여 내용을 볼수 있다. */
+            serverHost = new WCFTcpServerClient(
+                ict,
+                bindig,
+                new EndpointAddress("net.tcp://150.1.13.166/WCFTcpServer"));
+
+            //호스트에 접속한다. 만약 호스트가 실행중이 아니라면 Exception..
+            serverHost.Open();
+            serverHost.StartService();
+        }
+
+        void DisconnectWCFService()
+        {
+            if (serverHost == null)
+            {
+                return;
+            }
+
+            if (serverHost.State == CommunicationState.Opened)
+            {
+                serverHost.StopService();
+                serverHost.Close();
+            }
+
+            serverHost = null;
+        }
+        
+        /// <summary>
+        /// 호스트 로 부터 메시지를 받을 콜백 클래스
+        /// </summary>>
+        class ServerCallbackHandler : WCFTcpServer.IWCFTcpServerCallback
+        {
+            public void SetDataToClient(WCFMessageKind messageKind, object data)
+            {
+                MainForm.Invoke(new MethodInvoker(delegate ()
+                {
+                    switch (messageKind)
+                    {
+                        case WCFMessageKind.Message:
+                            MainForm.textBoxMessage.Text = data.ToString();
+                            break;
+                    }
+                }));
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serverHost == null)
+            {
+                return;
+            }
+
+            serverHost.SetDataToServer(textBoxMessage.Text);
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            ConnectWCFService();
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            DisconnectWCFService();
+        }
+    }
+}
